fe756aeb803fa03d8d87fe22e773ba8a7bcbad39
Michal Milko <tcom.milko@gmail.com>
2022-01-29 21:05:59 +0100

    mdetailing.sk

---
+++ script/script.js
@@ -0,0 +1,194 @@
+(function($){
+   $(function(){
+        // KED SA STLACI MENU TLACITKO.. OTVORI MINI MENU
+            $(".mobilikonazmena").click(function()
+               {
+                 $(".navigacia2").fadeToggle();
+               });
+            // KED SA STLACI MENU TLACITKO ZMENI SA NA X ALEBO OPACNE
+             $(".mobilikonazmena").click(function(){
+
+                 if($(".zmenaobrazka").attr("src") == "image/startmenu.png")
+                  {
+                     $( $(".zmenaobrazka").attr("src","image/stopmenu.png"));
+                  }
+                 else
+                  {
+                     $( $(".zmenaobrazka").attr("src","image/startmenu.png"));
+                  }
+                });
+         });
+    })(jQuery);
+       le
+// tlacitka v cenniku na zobrazenie viac..
+                     //prvy rad
+                        function zobrazitviac1(){
+                           if(document.getElementById("viacinfo1").style.display == "flex"){
+                           let viacinfoo= document.getElementById("viacinfo1");
+                           viacinfoo.style.display="none";
+                           let tlacitkoo= document.getElementById("tlacitkoviac1");
+                           tlacitkoo.style.background="#d3de53";
+                           tlacitkoo.innerText="VIAC O SLUŽBE";
+                           }
+                           else{
+                           let viacinfoo= document.getElementById("viacinfo1");
+                           viacinfoo.style.display="flex";
+                           let tlacitkoo= document.getElementById("tlacitkoviac1");
+                           tlacitkoo.style.background="#ED6663";
+                           tlacitkoo.style.border="red";
+                           tlacitkoo.innerText="MENEJ O SLUŽBE";
+                           }
+                           }
+
+                        function zobrazitviac2(){
+                           if(document.getElementById("viacinfo2").style.display == "flex"){
+                           let viacinfoo= document.getElementById("viacinfo2");
+                           viacinfoo.style.display="none";
+                           let tlacitkoo= document.getElementById("tlacitkoviac2");
+                           tlacitkoo.style.background="#d3de53";
+                           tlacitkoo.innerText="VIAC O SLUŽBE";
+                           }
+                           else{
+                           let viacinfoo= document.getElementById("viacinfo2");
+                           viacinfoo.style.display="flex";
+                           let tlacitkoo= document.getElementById("tlacitkoviac2");
+                           tlacitkoo.style.background="#ED6663";
+                           tlacitkoo.style.border="red";
+                           tlacitkoo.innerText="MENEJ O SLUŽBE";
+                           }
+                           }
+
+                        function zobrazitviac3(){
+                           if(document.getElementById("viacinfo3").style.display == "flex"){
+                           let viacinfoo= document.getElementById("viacinfo3");
+                           viacinfoo.style.display="none";
+                           let tlacitkoo= document.getElementById("tlacitkoviac3");
+                           tlacitkoo.style.background="#d3de53";
+                           tlacitkoo.innerText="VIAC O SLUŽBE";
+                           }
+                           else{
+                           let viacinfoo= document.getElementById("viacinfo3");
+                           viacinfoo.style.display="flex";
+                           let tlacitkoo= document.getElementById("tlacitkoviac3");
+                           tlacitkoo.style.background="#ED6663";
+                           tlacitkoo.style.border="red";
+                           tlacitkoo.innerText="MENEJ O SLUŽBE";
+                           }
+                           }
+                     //druhy rad 
+                        function zobrazitviac4(){
+                           if(document.getElementById("viacinfo4").style.display == "flex"){
+                           let viacinfoo= document.getElementById("viacinfo4");
+                           viacinfoo.style.display="none";
+                           let tlacitkoo= document.getElementById("tlacitkoviac4");
+                           tlacitkoo.style.background="#d3de53";
+                           tlacitkoo.innerText="VIAC O SLUŽBE";
+                           }
+                           else{
+                           let viacinfoo= document.getElementById("viacinfo4");
+                           viacinfoo.style.display="flex";
+                           let tlacitkoo= document.getElementById("tlacitkoviac4");
+                           tlacitkoo.style.background="#ED6663";
+                           tlacitkoo.style.border="red";
+                           tlacitkoo.innerText="MENEJ O SLUŽBE";
+                           }
+                           }
+
+                        function zobrazitviac5(){
+                           if(document.getElementById("viacinfo5").style.display == "flex"){
+                           let viacinfoo= document.getElementById("viacinfo5");
+                           viacinfoo.style.display="none";
+                           let tlacitkoo= document.getElementById("tlacitkoviac5");
+                           tlacitkoo.style.background="#d3de53";
+                           tlacitkoo.innerText="VIAC O SLUŽBE";
+                           }
+                           else{
+                           let viacinfoo= document.getElementById("viacinfo5");
+                           viacinfoo.style.display="flex";
+                           let tlacitkoo= document.getElementById("tlacitkoviac5");
+                           tlacitkoo.style.background="#ED6663";
+                           tlacitkoo.style.border="red";
+                           tlacitkoo.innerText="MENEJ O SLUŽBE";
+                           }
+                           }
+
+                        function zobrazitviac6(){
+                           if(document.getElementById("viacinfo6").style.display == "flex"){
+                           let viacinfoo= document.getElementById("viacinfo6");
+                           viacinfoo.style.display="none";
+                           let tlacitkoo= document.getElementById("tlacitkoviac6");
+                           tlacitkoo.style.background="#d3de53";
+                           tlacitkoo.innerText="VIAC O SLUŽBE";
+                           }
+                           else{
+                           let viacinfoo= document.getElementById("viacinfo6");
+                           viacinfoo.style.display="flex";
+                           let tlacitkoo= document.getElementById("tlacitkoviac6");
+                           tlacitkoo.style.background="#ED6663";
+                           tlacitkoo.style.border="red";
+                           tlacitkoo.innerText="MENEJ O SLUŽBE";
+                           }
+                           }
+                      //treti rad
+                        function zobrazitviac7(){
+                           if(document.getElementById("viacinfo7").style.display == "flex"){
+                           let viacinfoo= document.getElementById("viacinfo7");
+                           viacinfoo.style.display="none";
+                           let tlacitkoo= document.getElementById("tlacitkoviac7");
+                           tlacitkoo.style.background="#d3de53";
+                           tlacitkoo.innerText="VIAC O SLUŽBE";
+                           }
+                           else{
+                           let viacinfoo= document.getElementById("viacinfo7");
+                           viacinfoo.style.display="flex";
+                           let tlacitkoo= document.getElementById("tlacitkoviac7");
+                           tlacitkoo.style.background="#ED6663";
+                           tlacitkoo.style.border="red";
+                           tlacitkoo.innerText="MENEJ O SLUŽBE";
+                           }
+                           }
+
+                        function zobrazitviac8(){
+                           if(document.getElementById("viacinfo8").style.display == "flex"){
+                           let viacinfoo= document.getElementById("viacinfo8");
+                           viacinfoo.style.display="none";
+                           let tlacitkoo= document.getElementById("tlacitkoviac8");
+                           tlacitkoo.style.background="#d3de53";
+                           tlacitkoo.innerText="VIAC O SLUŽBE";
+                           }
+                           else{
+                           let viacinfoo= document.getElementById("viacinfo8");
+                           viacinfoo.style.display="flex";
+                           let tlacitkoo= document.getElementById("tlacitkoviac8");
+                           tlacitkoo.style.background="#ED6663";
+                           tlacitkoo.style.border="red";
+                           tlacitkoo.innerText="MENEJ O SLUŽBE";
+                           }
+                           }
+
+                        function zobrazitviac9(){
+                           if(document.getElementById("viacinfo9").style.display == "flex"){
+                           let viacinfoo= document.getElementById("viacinfo9");
+                           viacinfoo.style.display="none";
+                           let tlacitkoo= document.getElementById("tlacitkoviac9");
+                           tlacitkoo.style.background="#d3de53";
+                           tlacitkoo.innerText="VIAC O SLUŽBE";
+                           }
+                           else{
+                           let viacinfoo= document.getElementById("viacinfo9");
+                           viacinfoo.style.display="flex";
+                           let tlacitkoo= document.getElementById("tlacitkoviac9");
+                           tlacitkoo.style.background="#ED6663";
+                           tlacitkoo.style.border="red";
+                           tlacitkoo.innerText="MENEJ O SLUŽBE";
+                           }
+                           }
+
+
+                           function skusimalert(){
+                              window.open("", "", "width=200,height=100");
+                           }
+
+      
+   
+     
